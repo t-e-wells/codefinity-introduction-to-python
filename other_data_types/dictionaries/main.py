ef147c69f221d2aa976b32c5116bb0ccc40ab037
t-e-wells <>
2026-05-25 22:01:33 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,11 @@
+grocery_inventory = {"Milk" : (113,"Dairy"),
+                    "Eggs" : (116,"Dairy"),
+                    "Bread" : (117,"Bakery"),
+                    "Apples" : (141,"Produce")}
+
+bread_details = grocery_inventory.get("Bread")
+print("Details of Bread:", grocery_inventory.get("Bread"))
+grocery_inventory.update({"Cookies" : (143,"Bakery")})
+print("Inventory after adding Cookies: ", grocery_inventory)
+grocery_inventory.pop("Eggs")
+print("Inventory after removing Eggs: ", grocery_inventory)
